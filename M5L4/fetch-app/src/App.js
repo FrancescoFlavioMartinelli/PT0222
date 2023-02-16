4768d91dcf405e991207a2359394f39fa055bfd4
Flavio <=>
2023-02-16 21:58:42 +0100

    M5L4

---
+++ M5L4/fetch-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Eshop } from './Eshop';
+
+function App() {
+  return (
+    <div className="App">
+      <Eshop></Eshop>
+    </div>
+  );
+}
+
+export default App;
